496aae33f87fd8bced1ebf4f5bfc528afe3f4925
Prince989 <alireza1377aj@gmail.com>
2021-11-04 13:37:58 +0330

    Add files via upload

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ const adminRouter = require("./api/admin/admin.router");
 const authRouter = require("./api/auth/login/login.router");
 const fileUpload = require('express-fileupload');
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type','authorization'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+  }));
 let options = {
     redirect: false
 }
 app.use(express.static('public', options));
+// app.use(express.bodyParser({limit: '50mb'}));
 
 app.use(fileUpload({
     useTempFiles: true,
@@ -22,7 +29,10 @@ app.use(fileUpload({
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/auth",authRouter);
-const port = 8080;
+const port = 33140;
+
+const JDate = require('jalali-date');
+
 
 app.listen(port, () => {
     console.log("server up and running on PORT :", port);
